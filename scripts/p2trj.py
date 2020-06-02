5635f1a1c2a487696784d738ca0292faa4b24f06
fabothch <49951809+fabothch@users.noreply.github.com>
2020-06-02 12:07:32 +0200

    added copyright stuff

--- scripts/p2trj.py
+++ scripts/p2trj.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+# This file is part of ENSO.                                                         
+# Copyright (C) 2020 Fabian Bohle
+#                                                                                    
+# ENSO is free software: you can redistribute it and/or modify it under              
+# the terms of the GNU Lesser General Public License as published by                 
+# the Free Software Foundation, either version 3 of the License, or                  
+# (at your option) any later version.                                                
+#                                                                                    
+# ENSO is distributed in the hope that it will be useful,                            
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                     
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                       
+# GNU Lesser General Public License for more details.                                
+#                                                                                    
+# You should have received a copy of the GNU Lesser General Public License           
+# along with ENSO. If not, see <https://www.gnu.org/licenses/>.                      
+
+
+
 """ generate structure-ensemble from  ouputfiles and conformer folders"""
-
-
 
 from sys import argv,exit
 import os
--- scripts/update_old.sh
+++ scripts/update_old.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+# This file is part of ENSO.
+# Copyright (C) 2020 Fabian Bohle
+#
+# ENSO is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ENSO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ENSO. If not, see <https://www.gnu.org/licenses/>.
+
+
 # The script is intended to update enso.json files prepared by enso.py below version 2.0
 cp enso.json updated_enso.json
+mv enso.json old_enso.json
 
 sed -i 's/"nstruc":/"nconf":/g' updated_enso.json
 
@@ -28,3 +46,4 @@ if [ "$rrho" == 'xtb' ]; then
     sed -i 's/"rrho": "not_calculated"/"rrho_xtb": "not_calculated"/g'  updated_enso.json
     sed -i 's/"energy_rrho":/"energy_rrho_xtb":/g' updated_enso.json
 fi
+cp updated_enso.json enso.json
